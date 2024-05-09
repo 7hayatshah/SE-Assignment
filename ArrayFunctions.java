05d7262e3c3739af7449802473dfbecab52430be
javeenkumar <javeenkumarm@gmail.com>
2024-05-09 11:51:46 +0500

    add Scanner class to take input

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class ArrayFunctions{
     public static void main(String[] args) {
         //Creating An Array
-        int[] Array = new int[12];
+        Scanner ob=new Scanner(System.in);
+		System.out.println("Enter 10 num");
+		int num[]=new int[10];
+		for(int i=0; i<10; i++){
+			num[i]=ob.nextInt();
+		}
 
     }
 }
