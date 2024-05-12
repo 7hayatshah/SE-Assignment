39890b3d1f51e0303b0cfb6241f6dd53a4b1e7b7
7hayatshah <hayatshah1481947@gmail.com>
2024-05-12 18:11:15 +0500

    Created a loop for our switch case block

--- Main.java
+++ Main.java
@@ -9,24 +9,32 @@ public class Main{
 		//Creating An Array and filling it with random numbers!
 		int[] num = new int[10];
 		fs.generate(num);
-		System.out.println("Array filled Random Numbers between 1 to 25!");
+		System.out.println("- Array filled Random Numbers between 1 to 25!");
 
-			//Using switch to let user choose what he wants to do!
-			System.out.println("Press the number related to your desired function!");
-			System.out.println("Display: 1 \nSearch:  2 ");
-			switch(ob.nextInt()){
-			case 1: 
-			//Display Array Contents
-			System.out.println("Following are the Array contents:");
-			for(int i=0; i<10; i++)
-			System.out.println(num[i]);
-			break;
+		boolean a = true;
+		while(a){
+		//Using switch to let user choose what he wants to do!
+		System.out.println("- Press the number related to your desired function!");
+		System.out.println("Display: 1 \nSearch:  2 ");
+		switch(ob.nextInt()){
+			
+		case 1:
+		//Display Array Contents
+		for(int i=0; i<10; i++)
+		System.out.println(num[i]);
+		break;
 
-			case 2: 
-			//searching
-			System.out.println("Enter the number you want to search:");
-			fs.search(ob.nextInt(), num);
+		case 2:
+		//Searching
+		System.out.println("Insert the number you want to search");
+		fs.search(ob.nextInt(), num);
+		break;
+		}
+
+		//Taking user input to see if he wants to continue
+		System.out.println("\nPress '0' if you want to exit or other number if you want to continue...");
+		if(ob.nextInt()==0)
 			break;
-			}
 	}
+}
 }
